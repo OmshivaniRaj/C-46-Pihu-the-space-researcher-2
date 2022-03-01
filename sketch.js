0ca011d52f4c4203f8a682b33e527143f64360ea
OmshivaniRaj <84126951+OmshivaniRaj@users.noreply.github.com>
2022-03-01 21:30:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,16 +15,16 @@ var gameState = "fight"
 
 function preload(){
   
-  heart1Img = loadImage("assets/heart_1.png")
-  heart2Img = loadImage("assets/heart_2.png")
-  heart3Img = loadImage("assets/heart_3.png")
+  heart1Img = loadImage("heart_1.png")
+  heart2Img = loadImage("heart_2.png")
+  heart3Img = loadImage("heart_3.png")
 
-  shooterImg = loadImage("assets/Astro4.jpeg")
-  shooter_shooting = loadImage("assets/Astro3.jpeg")
+  shooterImg = loadImage("Astro4.jpeg")
+  shooter_shooting = loadImage("Astro3.jpeg")
 
-  zombieImg = loadImage("assets/ASTEROID.jpeg")
+  zombieImg = loadImage("ASTEROID.jpeg")
 
-  bgImg = loadImage("assets/BG 3.jpeg")
+  bgImg = loadImage("BG 3.jpeg")
 
 }
 
